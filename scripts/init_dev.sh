9d5de64ed558cd1facb3489bae754b01b9884116
Guillaume Malbrand <gmalbrand@free.fr>
2025-09-03 20:07:28 +0200

    Fixing docker env file

--- scripts/init_dev.sh
+++ scripts/init_dev.sh
@@ -10,7 +10,7 @@ echo "Creating directory structure"
 mkdir -p ./logs ./plugins ./config
 
 echo "Creating docker env file with Airflow UID"
-echo -e "AIRFLOW_UID=$(id -u)" > .env
+echo "AIRFLOW_UID=$(id -u)" > .env
 
 echo "Downloading docker-compose for Airflow 3.0.6"
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/3.0.6/docker-compose.yaml'
